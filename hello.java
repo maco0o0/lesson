796bec00b12f5942225764861afc39694718db2f
iwasaki <iwasaki@192.168.11.5>
2017-08-20 17:40:35 +0900

    a

--- hello.java
+++ hello.java
@@ -4,10 +4,21 @@ public class hello {
 		int i;
 		if (i % 3 == 0 && i % 5 == 0) {
 			System.out.println("");
-		}else if(i%3==0){
-			System.out.println("");
-		}else if (i%5==0) {
-			System.out.println("");
+		} else if (i % 3 == 0) {
+			System.out.println("a");
+		} else if (i % 5 == 0) {
+			System.out.println("b");
+		}
+		multiply();
+	}
+
+	public void multiply() {
+		for (int x = 1; x < 10; x++) {
+			for (int y = 1; y < 10; y++) {
+				System.out.print(x * y);
+				System.out.print(" ");
+			}
+			System.out.println();
 		}
 	}
 }
